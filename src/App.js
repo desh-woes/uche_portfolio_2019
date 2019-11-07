cb16b1a059641ba897ebb7ae2457a1af59a514f4
desh-woes <ksadesina@gmail.com>
2019-11-07 17:51:58 +0200

    Added scrollable component to enable new page scroll to the top.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Header2 from "./components/hamburger_header";
 import Header from "./components/header_comp";
+import ScrollToTop from "./components/scrollToTop";
 
 // Import required pages
 import RenderHome from "./pages/home_page";
@@ -14,22 +15,25 @@ import RenderLetterMark from "./pages/letter_mark";
 // Import Style sheet
 import "./style/index.css";
 
+
 function App() {
     return(
         <Router>
-            <div>
-                <Header/>
-                <Header2/>
-                <Switch>
-                    <Route path={"/"} exact component={RenderHome}/>
-                    <Route path={"/work"} component={RenderHome}/>
-                    <Route path={"/about"} component={RenderAboutPage}/>
-                    <Route path={"/paga"} component={RenderPagaPage}/>
-                    <Route path={"/zap"} component={RenderZap}/>
-                    <Route path={"/letterMark"} component={RenderLetterMark}/>
-                    <Route path={"/agriPredict"} component={RenderAgriPredict}/>
-                </Switch>
-            </div>
+            <ScrollToTop>
+                <div>
+                    <Header/>
+                    <Header2/>
+                    <Switch>
+                        <Route path={"/"} exact component={RenderHome}/>
+                        <Route path={"/work"} component={RenderHome}/>
+                        <Route path={"/about"} component={RenderAboutPage}/>
+                        <Route path={"/paga"} component={RenderPagaPage}/>
+                        <Route path={"/zap"} component={RenderZap}/>
+                        <Route path={"/letterMark"} component={RenderLetterMark}/>
+                        <Route path={"/agriPredict"} component={RenderAgriPredict}/>
+                    </Switch>
+                </div>
+            </ScrollToTop>
         </Router>
     )
 }
